45d270b7e6a80faf7ba9b2e298733642975a49b2
Charlie Llewellin <charlie@llewellin.net>
2018-04-18 16:14:58 -0500

    cni theme cleanup

--- js/etype.js
+++ js/etype.js
@@ -1,1 +1,8 @@
 /* add custom js */
+(function ($) {
+  "use strict";
+  Drupal.behaviors.md_thenews = {
+    attach: function (context, settings) {
+      $("a[href$='e-edition']").attr('target', '_blank');
+    };
+})(jQuery);
