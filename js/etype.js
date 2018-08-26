9fabe7f10946ffff11066c1151c1e854067c7565
Charlie Llewellin <charlie@llewellin.net>
2018-08-26 09:58:33 +0100

    autocomplete off for contact form

--- js/etype.js
+++ js/etype.js
@@ -6,11 +6,11 @@
             $('a[href$="e-edition"]').attr("target", "_blank");
 
             var autocompleteOff = function () {
-              var field = "autocomplete";
-              var fill = "nope";
-              $("#contact-site-form").attr(field, fill);
-              $('#contact-site-form .form-item input[type="text"]').attr(field, fill);
-              $('#contact-site-form .form-item textarea').attr(field, fill);
+                var field = "autocomplete";
+                var fill = "nope";
+                $("#contact-site-form").attr(field, "off");
+                $('#contact-site-form .form-item input[type="text"]').attr(field, fill);
+                $("#contact-site-form .form-item textarea").attr(field, fill);
             };
             autocompleteOff();
         }
