c9fd4e4e88484b3af8f5f5bb135d5113a2f7cc3f
Charlie Llewellin <charlie@llewellin.net>
2018-08-26 10:05:18 +0100

    autocomplete off for contact form

--- js/etype.js
+++ js/etype.js
@@ -4,6 +4,15 @@
     Drupal.behaviors.etype = {
          attach: function (context, settings) {
             $('a[href$="e-edition"]').attr("target", "_blank");
+
+            var autocompleteOff = function () {
+                var field = "autocomplete";
+                var fill = "nope";
+                $("#contact-site-form").attr(field, "off");
+                $('#contact-site-form .form-item input[type="text"]').attr(field, fill);
+                $("#contact-site-form .form-item textarea").attr(field, fill);
+            };
+            autocompleteOff();
         }
     };
 })(jQuery);
