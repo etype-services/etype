d5a285cecf323b2cca3c954e3738336ece4c7db9
Charlie Llewellin <charlie@llewellin.net>
2018-10-10 15:35:19 -0500

    white space no-wrap for social text in footer

--- js/etype.js
+++ js/etype.js
@@ -1,18 +1,20 @@
 /* add custom js */
 (function ($) {
-    "use strict";
-    Drupal.behaviors.etype = {
-         attach: function (context, settings) {
-            $('a[href$="e-edition"]').attr("target", "_blank");
+  'use strict';
+  Drupal.behaviors.etype = {
+    attach: function (context, settings) {
+      $('a[href$="e-edition"]').attr('target', '_blank');
 
-            var autocompleteOff = function () {
-                var field = "autocomplete";
-                var fill = "nope";
-                $("#contact-site-form").attr(field, "off");
-                $('#contact-site-form .form-item input[type="text"]').attr(field, fill);
-                $("#contact-site-form .form-item textarea").attr(field, fill);
-            };
-            autocompleteOff();
+      var autocompleteOff = function () {
+        if ($('#contact-site-form').length) {
+          var field = 'autocomplete';
+          var fill = 'nope';
+          $('#contact-site-form').attr(field, 'off');
+          $('#contact-site-form .form-item input[type="text"]').attr(field, fill);
+          $('#contact-site-form .form-item textarea').attr(field, fill);
         }
-    };
+      };
+      autocompleteOff();
+    }
+  };
 })(jQuery);
